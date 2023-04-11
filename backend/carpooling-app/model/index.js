b22e113bef19674fa92b28e2b062596ffebbd95b
Vaishnavi200029 <bhoite.v@northeastern.edu>
2023-04-11 15:19:18 -0700

    changes

--- backend/carpooling-app/model/index.js
+++ backend/carpooling-app/model/index.js
@@ -1,9 +1,11 @@
 import Driver from './driver.js';
 import Rider from './rider.js';
 import RiderOrder from './riderOrder.js';
+import UserAuth from './UserAuth.js';
 
 export default{
     Driver,
     Rider,
-    RiderOrder
+    RiderOrder,
+    UserAuth
 }
--- backend/carpooling-app/routes/index.js
+++ backend/carpooling-app/routes/index.js
@@ -1,11 +1,13 @@
 import driverRouter from './driver-route.js';
 import riderRouter from './rider-route.js';
 import riderOrderRouter from './riderOrder-route.js';
-
+import userAuthRouter from './userAuth-route.js';
+ 
 const route = (app) => {
     app.use('/drivers',driverRouter);
     app.use('/riders',riderRouter);
     app.use('/riderOrders',riderOrderRouter);
+    app.use('/userAuths',userAuthRouter);
 
 }
 
